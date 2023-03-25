d50d2895c88c567e5313b8f171725e3a7d0d2e07
Máté Horváth <mahte007@gmail.com>
2023-03-25 19:05:20 +0100

    Backend api succesfully connected to frontend

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -8,9 +8,10 @@ interface InputProps {
   label?: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   register?: any;
+  showerror?: boolean;
 }
 
-export const Input: FC<InputProps> = ({ name, type, label, register }) => {
+export const Input: FC<InputProps> = ({ name, type, label, register, showerror }) => {
   return (
     <>
       <label htmlFor={name}>{label}</label>
--- src/components/registerForm/registerForm.tsx
+++ src/components/registerForm/registerForm.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/jsx-props-no-spreading */
 import type { FC } from 'react';
+import { useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { useForm } from 'react-hook-form';
 import type { SubmitHandler } from 'react-hook-form';
+import { json } from 'stream/consumers';
 
 import BackGround from 'components/background/BackGround';
 import { Input } from 'components/input/input';
@@ -12,14 +16,67 @@ interface FormData {
   name: string;
   email: string;
   password: string;
-  passwordConfirm: string;
+  confirm_password: string;
 }
 
 export const RegisterForm: FC = () => {
-  const { register, handleSubmit } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<FormData>();
+  const [showError, setShowError] = useState(false);
+  const [isSuccessful, setIsSuccessful] = useState(false);
+  const url = 'api/registration';
 
   const formSubmitHandler: SubmitHandler<FormData> = data => {
-    console.log(data);
+    async function fetchData() {
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      };
+
+      const response = await window.fetch(url, requestOptions);
+      const json = await response.json();
+
+      if (response.ok) {
+        setIsSuccessful(true);
+      } else if (response.status === 406) {
+        if (json.errors.name) {
+          setError('name', {
+            type: 'server',
+            message: json.errors.name.message,
+          });
+        }
+        if (json.errors.email) {
+          setError('email', {
+            type: 'server',
+            message: json.errors.email.message,
+          });
+        }
+        if (json.errors.password.rule !== 'same') {
+          setError('password', {
+            type: 'server',
+            message: json.errors.password.message,
+          });
+        }
+        if (json.errors.password.rule === 'same') {
+          setError('confirm_password', {
+            type: 'server',
+            message: json.errors.password.message,
+          });
+        }
+      } else {
+        console.log('Unexpected error: ', json);
+      }
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    fetchData();
   };
 
   function wrapAsyncFunction<ARGS extends unknown[]>(
@@ -43,18 +100,35 @@ export const RegisterForm: FC = () => {
           className="form-container"
           onSubmit={wrapAsyncFunction(handleSubmit(formSubmitHandler))}
         >
-          <Input label="Name" name="name" register={register} type="text" />
+          <Input label="Name" name="name" register={register} showerror={showError} type="text" />
+          <div className="error-text">{errors.name?.message}</div>
           <br />
-          <Input label="E-mail" name="email" register={register} type="email" />
+          <Input
+            label="E-mail"
+            name="email"
+            register={register}
+            showerror={showError}
+            type="email"
+          />
+          <div className="error-text">{errors.email?.message}</div>
           <br />
-          <Input label="Password" name="password" register={register} type="password" />
+          <Input
+            label="Password"
+            name="password"
+            register={register}
+            showerror={showError}
+            type="password"
+          />
+          <div className="error-text">{errors.password?.message}</div>
           <br />
           <Input
             label="Confirm Password"
-            name="passwordConfirm"
+            name="confirm_password"
             register={register}
+            showerror={showError}
             type="password"
           />
+          <div className="error-text">{errors.confirm_password?.message}</div>
           <br />
           <div className="tos-checkbox">
             <Input name="tos" register={register} type="checkbox" />
@@ -63,6 +137,7 @@ export const RegisterForm: FC = () => {
             </label>
           </div>
           <button type="submit">Register</button>
+          {isSuccessful && <p className="successful-message">Successful Registration</p>}
           <div className="sing-in-container">
             <p>Already have an account?</p>
             <p>Sign in</p>
